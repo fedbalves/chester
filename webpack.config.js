23ef1c6e8ec24ff1ea4130ebea02b1502e2b4fe1
Fernando Barros <fedbalves@gmail.com>
2020-08-05 21:02:43 -0300

    INI COMMIT

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const webpack = require('webpack');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const GitRevisionPlugin = require('git-revision-webpack-plugin');
+
+const gitRevisionPlugin = new GitRevisionPlugin({ branch: true });
+
+module.exports = (env) => ({
+  entry: ['@babel/polyfill', './src/index.tsx'],
+
+  output: {
+    filename: '[git-revision-BRANCH]-[git-revision-VERSION].js',
+    path: path.join(__dirname, '/dist'),
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+
+  devServer: {
+    compress: true,
+    port: 8081,
+    contentBase: './public',
+    publicPath: '/',
+    historyApiFallback: true,
+    hot: true,
+    open: false,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(otf)$/,
+        use: {
+          loader: 'url-loader',
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new Dotenv({
+      path: `./.env.${env.NODE_ENV}`,
+      safe: true,
+      silent: true,
+    }),
+
+    gitRevisionPlugin,
+
+    new webpack.DefinePlugin({
+      VERSION: JSON.stringify(gitRevisionPlugin.version()),
+      COMMITHASH: JSON.stringify(gitRevisionPlugin.commithash()),
+      BRANCH: JSON.stringify(gitRevisionPlugin.branch()),
+    }),
+
+    new HtmlWebpackPlugin({
+      title: 'FanáticoFC',
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
+});
